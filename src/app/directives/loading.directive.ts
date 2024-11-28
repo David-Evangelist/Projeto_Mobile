3adadcbd29fcf11ef4b4b08227d9499ffdad0960
David-Evangelist <dev.davidwilliam@gmail.com>
2024-11-28 17:28:58 -0300

    Ajustes na implementação das diretivas personalizadas

--- src/app/directives/loading.directive.ts
+++ src/app/directives/loading.directive.ts
@@ -1,7 +1,7 @@
 import { Directive, Input, Renderer2, ElementRef } from '@angular/core';
 
 @Directive({
-  selector: '[appLoading]'
+  selector: '[appLoading]',
 })
 export class LoadingDirective {
   @Input() set appLoading(isLoading: boolean) {
@@ -16,10 +16,17 @@ export class LoadingDirective {
 
   constructor(private el: ElementRef, private renderer: Renderer2) {
     this.loaderElement = this.renderer.createElement('div');
+    this.renderer.addClass(this.loaderElement, 'loading-container');
+
     this.loaderElement.innerHTML = `
       <ion-spinner name="crescent"></ion-spinner>
-      <p>Carregando...</p>`;
-    this.renderer.addClass(this.loaderElement, 'loading-container');
+      <p>Carregando...</p>
+    `;
+
+    this.renderer.setStyle(this.loaderElement, 'display', 'flex');
+    this.renderer.setStyle(this.loaderElement, 'align-items', 'center');
+    this.renderer.setStyle(this.loaderElement, 'justify-content', 'center');
+    this.renderer.setStyle(this.loaderElement, 'height', '100%');
   }
 
   private showLoader() {
